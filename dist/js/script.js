74b04de300bc8d23324983e079483f406a9d2b1f
Oleg <olegjan9@rambler.ru>
2021-10-23 22:52:55 +0300

    23.10.2021

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,9 +5,6 @@ const hamburgerLine = document.querySelectorAll(".hamburger__line"),
     hamburger = document.querySelector(".hamburger"),
     container = document.querySelector(".container");
 
-
-
-
 function show(selector) {
     document.querySelector(selector).classList.remove("hide");
     document.querySelector(selector).classList.add("show");
@@ -66,58 +63,3 @@ form.forEach(function (item, i) {
     });
 });
 
-
-const body = document.body;
-
-// Определение, заходит ли с мобильного устройства
-// если заходим с декстопа, то тэгу body присваивается класс mouse
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    // код для мобильных устройств
-    document.body.classList.remove("mouse");
-    document.body.classList.add("mobile");
-    // scale.forEach(item => {
-
-    // })
-
-} else {
-    // код для обычных устройств
-    document.body.classList.add("mouse");
-    document.body.classList.remove("mobile");
-}
-
-const dropDown = document.querySelector(".drop-down"),
-    topmenu = dropDown.querySelector(".topmenu"),
-    a = dropDown.querySelectorAll("a"),
-    submenu = dropDown.querySelectorAll(".submenu"),
-    home = dropDown.querySelectorAll(".home");
-
-a.forEach((item) => {
-    item.addEventListener("click", (e) => {
-        e.preventDefault();
-    });
-});
-
-// Событие на оболочку меню
-dropDown.addEventListener("click", (e) => {
-    // Перебираем элементы меню, на которые будем кликать, 
-    // чтобы их порядковый номер присвоить к содержемуся
-    // в нем подменю
-    home.forEach((item, i) => {
-        // Вешаем событие на каждый элемент меню     
-        if (e.target && e.target === item) {
-            // Скрываем все подменю
-            submenu.forEach((item) => {
-                item.style.cssText = "transform: scaleY(0)";
-            })
-            // Показываем только то подменю, которого индекс совпадает
-            // с его родителем - элементом меню
-            submenu[i].style.cssText = "transform: scaleY(1)";
-        }
-    })
-    // Скрываем элементы подменю при нажатии на любое свободное пространство
-    if (e.target.matches(".drop-down") || e.target.matches(".topmenu")) {
-        submenu.forEach((item) => {
-            item.style.cssText = "transform: scaleY(0)";
-        })
-    }
-})
